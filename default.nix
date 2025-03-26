c87211b3a411cd5288b3e11da646bd029849de0b
AnimusNull <animusnull@gmail.com>
2025-03-26 13:38:38 +0000

    adding gitea action;

--- default.nix
+++ default.nix
@@ -8,32 +8,26 @@
     # Use <Space> as leader key
     globals.mapleader = " ";
     extraConfigLua = ''
-    vim.api.nvim_set_hl(0, "CmpItemAbbrSemiboldUnderline", {
-      bold = true,
-      underline = true,
-    })
-    vim.api.nvim_set_hl(0, "CmpItemKindLightItalic", {
-      italic = true,
-    })
-    ${builtins.readFile ./lua/utils.lua}
+      vim.api.nvim_set_hl(0, "CmpItemAbbrSemiboldUnderline", {
+        bold = true,
+        underline = true,
+      })
+      vim.api.nvim_set_hl(0, "CmpItemKindLightItalic", {
+        italic = true,
+      })
+      ${builtins.readFile ./lua/utils.lua}
     '';
-
     # Set 'vi' and 'vim' aliases to nixvim
     viAlias = true;
     vimAlias = true;
-
     # Setup clipboard support
     clipboard = {
       # Use xsel as clipboard provider
       providers.xsel.enable = true;
-
       # Sync system clipboard
       register = "unnamedplus";
     };
-    plugins = {
-      zellij.enable = true;
-
-    };
+    plugins = { zellij.enable = true; };
     extraPlugins = with pkgs.vimPlugins; [
       lspkind-nvim
       mini-icons
--- plugins/easy-dotnet.nix
+++ plugins/easy-dotnet.nix
@@ -2,9 +2,17 @@
   opts = {
     enable = true;
     package = pkgs.unstable.vimPlugins.easy-dotnet-nvim;
-    settings = {
-
-    };
+    settings = { };
+    luaConfig.post = # lua
+      ''
+              if vim.bo.filetype == "cs" or vim.bo.filetype == "csharp" then
+              vim.keymap.set("n", "<leader>pe", "<cmd>DotnetUI<CR>", { buffer = true, desc = "Show Easy Dotnet UI" })
+              vim.keymap.set("n", "<leader>pb", "<cmd>Dotnet build quickfix<CR>", { buffer = true, desc = "Run dotnet build with quick fix" })
+              vim.keymap.set("n", "<leader>pr", "<cmd>Dotnet restore<CR>", { buffer = true, desc = "Run dotnet restore" })
+              vim.keymap.set("n", "<leader>pc", "<cmd>Dotnet clean<CR>", { buffer = true, desc = "Run dotnet clean" })
+              vim.keymap.set("n", "<leader>pf", "<cmd>split | terminal dotnet format<CR>", { buffer = true, desc = "Run dotnet format in terminal" })
+        end 
+      '';
   };
   rootOpts = { };
 }
